c8978f7f3c9019f4c024fbac6c3ef60182f08333
Ģirts <girts.bebrovskis@gmail.com>
2026-08-27 13:21:42 +0300

    Refactor uninstall script for NumpadDotFix

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,14 +1,14 @@
+$ErrorActionPreference = "SilentlyContinue"
+
+Get-Process NumpadDotFix | Stop-Process -Force
+
 Remove-ItemProperty `
     -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
-    -Name "NumpadDotFix" `
-    -ErrorAction SilentlyContinue
-
-Get-Process NumpadDotFix -ErrorAction SilentlyContinue | Stop-Process -Force
+    -Name "NumpadDotFix"
 
 Remove-Item `
     "$env:LOCALAPPDATA\NumpadDotFix" `
     -Recurse `
-    -Force `
-    -ErrorAction SilentlyContinue
+    -Force
 
 Write-Host "NumpadDotFix removed."
